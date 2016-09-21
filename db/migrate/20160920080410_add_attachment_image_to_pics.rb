cd09d1f29dd4935c06be3a27a2fa7d29b1e58208
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2016-09-21 16:45:35 -0700

    fix the migration

--- db/migrate/20160920080410_add_attachment_image_to_pics.rb
+++ db/migrate/20160920080410_add_attachment_image_to_pics.rb
@@ -1,11 +1,11 @@
 class AddAttachmentImageToPics < ActiveRecord::Migration
-  def self.up
-    change_table :pics do |t|
-      t.attachment :image
-    end
-  end
-
-  def self.down
-    remove_attachment :pics, :image
-  end
+  # def self.up
+  #   change_table :pics do |t|
+  #     t.attachment :image
+  #   end
+  # end
+  #
+  # def self.down
+  #   remove_column :pics, :attachment
+  # end
 end

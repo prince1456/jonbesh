6605ef6f393269a624e18c1026ea0256ba3282a2
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2016-09-21 23:37:32 -0700

    fix first pagedsd

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :pics
   resources :images
   resources :users
-  root 'pics#index'
+  root 'pics#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

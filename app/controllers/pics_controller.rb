1573bfa9eb43b26e2b48a882310213ded22a0069
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2016-09-21 23:59:50 -0700

    fix first page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -5,8 +5,7 @@ class PicsController < ApplicationController
   # GET /pics
   # GET /pics.json
   def index
-    @pics = Pic.all
-    # .order(created_at: :DESC).page(params[:page]).per(30)
+    @pics = Pic.all.order(created_at: :DESC).page(params[:page]).per(30)
    end
 
   # GET /pics/1
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :pics
   resources :images
   resources :users
-  root 'pics#new'
+  root 'pics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
